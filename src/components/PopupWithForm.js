0f975463ebb4a9d079ab7c0da81a5d9ae7a29067
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-23 20:17:11 +0300

    feat: add conditional rendering for submit button disabled attr

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -13,7 +13,7 @@ function PopupWithForm(props) {
         <h3 className="form__header">{props.title}</h3>
           <fieldset className="form__fieldset">
             {props.children}
-            <button type="submit" className="form__save-button">{props.buttonText}</button>
+            <button disabled={props.isSubmitDisabled} type="submit" className="form__save-button">{props.buttonText}</button>
           </fieldset>
           <button onClick={props.onClose} type="reset" className="popup__close-button" aria-label="закрыть форму"></button>
         </form>
